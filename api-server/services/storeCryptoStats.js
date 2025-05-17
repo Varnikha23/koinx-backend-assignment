2b46165f15f34ce2f321d524295b9438ac866ae3
Varnikha23 <varnikha234@gmail.com>
2025-05-17 09:49:47 +0530

    Update storeCryptoStats.js

--- api-server/services/storeCryptoStats.js
+++ api-server/services/storeCryptoStats.js
@@ -1,1 +1,36 @@
+const axios = require('axios');
+const CryptoStat = require('../models/CryptoStat');
+
+const coins = ['bitcoin', 'ethereum', 'matic-network'];
+
+async function storeCryptoStats() {
+  try {
+    const response = await axios.get('https://api.coingecko.com/api/v3/simple/price', {
+      params: {
+        ids: coins.join(','),
+        vs_currencies: 'usd',
+        include_market_cap: 'true',
+        include_24hr_change: 'true'
+      }
+    });
+
+    for (const coin of coins) {
+      const data = response.data[coin];
+      if (!data) continue;
+
+      await CryptoStat.create({
+        coin,
+        price: data.usd,
+        marketCap: data.usd_market_cap,
+        change24h: data.usd_24h_change
+      });
+    }
+
+    console.log('Crypto stats stored successfully.');
+  } catch (error) {
+    console.error('Error storing crypto stats:', error.message);
+  }
+}
+
+module.exports = storeCryptoStats;
 
