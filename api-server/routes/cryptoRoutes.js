d69ed4ebdb7e73d689da4a355d663b593bc66780
Varnikha23 <varnikha234@gmail.com>
2025-05-17 09:48:05 +0530

    Update cryptoRoutes.js

--- api-server/routes/cryptoRoutes.js
+++ api-server/routes/cryptoRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { getLatestStats, getDeviation } = require('../controllers/cryptoController');
+
+router.get('/stats', getLatestStats);
+router.get('/deviation', getDeviation);
+
+module.exports = router;
 
