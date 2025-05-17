3d10dca944c35b15b74215eb9ee30f878ad52c06
Varnikha23 <varnikha234@gmail.com>
2025-05-17 09:51:04 +0530

    Update subscriber.js

--- api-server/nats/subscriber.js
+++ api-server/nats/subscriber.js
@@ -1,1 +1,21 @@
+const { connect } = require('nats');
+const storeCryptoStats = require('../services/storeCryptoStats');
+
+async function setupSubscriber() {
+  try {
+    const nc = await connect({ servers: process.env.NATS_URL });
+    const sub = nc.subscribe('update-stats');
+
+    console.log('Subscribed to NATS topic: update-stats');
+
+    for await (const msg of sub) {
+      console.log(`Received message: ${msg.data}`);
+      await storeCryptoStats();
+    }
+  } catch (error) {
+    console.error('Error connecting to NATS:', error.message);
+  }
+}
+
+module.exports = setupSubscriber;
 
